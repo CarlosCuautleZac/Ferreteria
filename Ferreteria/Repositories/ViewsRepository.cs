f317536c7ec1084ade0102c2b6adbc76f38a4a0d
Carlos Cuautle <191G0235@rcarbonifera.tecnm.mx>
2022-02-20 22:48:06 -0600

    Creacion de repositorios de views de las bd

---
+++ Ferreteria/Repositories/ViewsRepository.cs
@@ -0,0 +1,29 @@
+﻿using Ferreteria.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferreteria.Repositories
+{
+    public class ViewsRepository
+    {
+        ferreteriaContext Context = new();
+
+        public IEnumerable<Vwdepartamentosordenado> GetVwDepartamentosOrdenados()
+        {
+            return Context.Vwdepartamentosordenados;
+        }
+
+        public IEnumerable<Vwlistadoentre500y100> GetvwListadEentre500Y1000()
+        {
+            return Context.Vwlistadoentre500y100s;
+        }
+
+        public IEnumerable<Vwlistadomenoralpromedio> GetvwProductosMenorAlPromedio ()
+        {
+            return Context.Vwlistadomenoralpromedios;
+        }
+    }
+}
---
+++ Ferreteria/ViewModels/ViewsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Ferreteria.Models;
+using Ferreteria.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferreteria.ViewModels
+{
+    class ViewsViewModel
+    {
+        ViewsRepository viewsrepository = new();
+
+        public List<Vwdepartamentosordenado> DepartamentosOrdenados { get; set; }
+
+        public List<Vwlistadoentre500y100> ProductosEntre500y1000 { get; set; }
+
+        public List<Vwlistadomenoralpromedio> ProductosMenorAlPromedio { get; set; }
+
+        public ViewsViewModel()
+        {
+            DepartamentosOrdenados = new(viewsrepository.GetVwDepartamentosOrdenados());
+            ProductosEntre500y1000 = new(viewsrepository.GetvwListadEentre500Y1000());
+            ProductosMenorAlPromedio = new(viewsrepository.GetvwProductosMenorAlPromedio());
+        }
+    }
+}
